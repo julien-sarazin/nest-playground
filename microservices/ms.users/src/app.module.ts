606226ce961cfd920a5abe8e0cf8a382766cf2c3
Julien Sarazin <julien.sarazin@me.com>
2019-04-03 01:16:34 +0200

    wip on middleware logger

--- microservices/ms.users/src/app.module.ts
+++ microservices/ms.users/src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { HealthModule } from './modules/health/health.module';
 import { NestConsulModule } from '@shared/modules/consul/src';
 import { CONSUL_CONFIG } from './config/consul.development';
+import { UsersController } from './modules/users/controller/users.controller';
+import * as morgan from 'morgan';
 
 @Module({
     imports: [
@@ -12,4 +14,9 @@ import { CONSUL_CONFIG } from './config/consul.development';
     ],
 })
 export class ApplicationModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+          .apply(morgan('tiny'))
+          .forRoutes(UsersController);
+    }
 }
